d5105e75a378493379fb8c981299153d264baa51
Joshua Loftin <joshua.loftin@aa.com>
2022-05-10 18:58:08 -0500

    add validator

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import * as notes from './notes'
 import * as utils from './utils'
+import validator from 'validator'
 
+console.log();
 console.log(utils.name);
 console.log(utils.add(1, 2));
 console.log(notes.getNotes());
+console.log(validator.isEmail('joshua.loftin@aa.com'))
+console.log(validator.isEmail('joshua.loftinaa.com'))
+console.log(validator.isURL('https://google.com'))
+console.log(validator.isURL('https://googlecom'))
+console.log();
